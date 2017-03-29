32058ad2adcbc650035e6dd8daf2e82388ac2782
Lukas Herman <lukashh6@gmail.com>
2017-03-28 22:42:06 -0400

    Refractor everything

---
+++ config.py
@@ -0,0 +1,14 @@
+from os.path import join, splitext
+import sys
+
+LOG_DIR = join('log', splitext(sys.argv[0])[0])
+TRAIN_DIR = join(LOG_DIR, 'train')
+TEST_DIR = join(LOG_DIR, 'test')
+N_TRAINS = 25
+BATCH_SIZE = 100
+
+WIDTH = 28
+HEIGHT = 28
+CHANNELS = 1
+FLAT = WIDTH * HEIGHT * CHANNELS
+N_CLASSES = 10
---
+++ layers/layers.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+
+def conv_layer(input, side, channels_in, channels_out, name='conv_layer'):
+    with tf.name_scope(name):
+        w = tf.Variable(tf.truncated_normal([side, side, channels_in,
+                        channels_out]), name='W')
+        b = tf.Variable(tf.truncated_normal([channels_out]),
+                        name='B')
+
+        conv =  tf.nn.conv2d(input, w, [1, 1, 1, 1], padding='SAME')
+        act = tf.nn.relu(conv + b)
+
+        tf.summary.histogram('weights', w)
+        tf.summary.histogram('biases', b)
+        return tf.nn.max_pool(act, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
+
+def fc_layer(input, channels_in, channels_out, name='fc_layer'):
+    with tf.name_scope(name):
+        w = tf.Variable(tf.truncated_normal([channels_in, channels_out]),
+                        name='W')
+        b = tf.Variable(tf.truncated_normal([channels_out]),
+                        name='B')
+
+        tf.summary.histogram('weights', w)
+        tf.summary.histogram('biases', b)
+        return tf.nn.relu(tf.matmul(input, w) + b)
+
+def output_layer(input, channels_in, channels_out, name='output_layer'):
+    with tf.name_scope(name):
+        w = tf.Variable(tf.truncated_normal([channels_in, channels_out]),
+                        name='W')
+        b = tf.Variable(tf.truncated_normal([channels_out]),
+                        name='B')
+
+        tf.summary.histogram('weights', w)
+        tf.summary.histogram('biases', b)
+        return tf.matmul(input, w) + b
