091056543ed86d053af63aed04b498778d4bf3ae
Lukas Herman <lukashh6@gmail.com>
2017-04-02 10:11:49 -0400

    Change n_trains to 10000

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import sys
 LOG_DIR = join('log', splitext(sys.argv[0])[0])
 TRAIN_DIR = join(LOG_DIR, 'train')
 TEST_DIR = join(LOG_DIR, 'test')
-N_TRAINS = 20
+N_TRAINS = 10000
 BATCH_SIZE = 100
 
 WIDTH = 28
